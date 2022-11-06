7ec4fd027cddc28ce8dc1f122897e765bc0fa867
Fausto Emmanuel Torres Diaz <faustoemmanueltorresdiaz@Faustos-MacBook-Pro.local>
2022-11-06 15:31:39 -0400

    configuring h2 db console

--- booking-api/src/main/java/com/hotel/booking/bookingapi/BookingApiApplication.java
+++ booking-api/src/main/java/com/hotel/booking/bookingapi/BookingApiApplication.java
@@ -1,13 +1,33 @@
 package com.hotel.booking.bookingapi;
 
+import com.hotel.booking.bookingapi.controller.UserController;
+import com.hotel.booking.bookingapi.entity.User;
+import com.hotel.booking.bookingapi.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookingApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApiApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner demo(UserRepository repository, UserController userController) {
+		return (args) -> {
+			// save a few customers
+			System.out.println(" >>>>>>>>>>>>>>>>>>>>>>> demo insert ");
+			User user = new User();
+			user.setUserName("Fausto");
+			repository.save(user);
+			System.out.println(" >>>>>>>>>>>>>>>>>>>>>>> demo insert end");
+
+			System.out.println(userController.getUsers());
+		};
 	}
 
 }
--- booking-api/src/main/java/com/hotel/booking/bookingapi/controller/UserController.java
+++ booking-api/src/main/java/com/hotel/booking/bookingapi/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.hotel.booking.bookingapi.controller;
 
+import com.hotel.booking.bookingapi.entity.User;
+import com.hotel.booking.bookingapi.repository.UserRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    public final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return (List<User>) this.userRepository.findAll();
+    }
 }

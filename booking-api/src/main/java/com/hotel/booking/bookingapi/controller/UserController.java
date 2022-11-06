bd26083d754db460a984cee6bc3c8bb660f65b83
Fausto Emmanuel Torres Diaz <faustoemmanueltorresdiaz@Faustos-MacBook-Pro.local>
2022-11-06 13:57:57 -0400

    creating reservation entity and entities relations

---
+++ booking-api/src/main/java/com/hotel/booking/bookingapi/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.hotel.booking.bookingapi.controller;
+
+public class UserController {
+}
---
+++ booking-api/src/main/java/com/hotel/booking/bookingapi/entity/Room.java
@@ -0,0 +1,76 @@
+package com.hotel.booking.bookingapi.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomName;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    @OneToOne(mappedBy = "room")
+    private Reservation reservation;
+
+    private Boolean available;
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Room{");
+        sb.append("id=").append(id);
+        sb.append(", roomName='").append(roomName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
